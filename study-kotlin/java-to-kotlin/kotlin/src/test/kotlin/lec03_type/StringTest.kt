a1a6bd5dc7b59e63e507881c961aad3e5d5bc7cb
ndy2 <emrdbs12@gmail.com>
2022-09-16 17:33:44 +0900

    [kotlin-jtk] lec03: string

---
+++ study-kotlin/java-to-kotlin/kotlin/src/test/kotlin/lec03_type/StringTest.kt
@@ -0,0 +1,31 @@
+package lec03_type
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StringTest {
+
+    @Test
+    internal fun `String InterPolation`() {
+        val name = "haha"
+        val age = 26
+
+        assertThat("이름 : ${name}, 나이 : $age").isEqualTo("이름 : haha, 나이 : 26")
+    }
+
+    @Test
+    internal fun `String Box`() {
+        val str = """
+            ABC
+            DEF
+        """.trimIndent()
+
+        assertThat(str).isEqualTo("ABCDEF")
+    }
+
+    @Test
+    internal fun `String Indexing`() {
+        val str = "ABCDEF"
+        assertThat(str[0]).isEqualTo('A')
+    }
+}
