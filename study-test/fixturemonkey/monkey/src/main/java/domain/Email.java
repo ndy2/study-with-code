47d650b894028878469a408eba24918c49fdab39
ndy2 <emrdbs12@gmail.com>
2022-09-18 21:01:45 +0900

    [test-fixturemonkey] test: email 생성 테스트

---
+++ study-test/fixturemonkey/monkey/src/main/java/domain/Email.java
@@ -0,0 +1,30 @@
+package domain;
+
+import static com.google.common.base.Preconditions.*;
+
+import java.beans.ConstructorProperties;
+import java.util.regex.Pattern;
+
+import lombok.Getter;
+
+@Getter
+public class Email {
+
+	private static final Pattern EMAIL_PATTERN = Pattern.compile(
+		"[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*"
+			+ "@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)"
+			+ "+[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]");
+
+	private String value;
+
+	public Email() {
+	}
+
+	@ConstructorProperties("value")
+	public Email(final String value) {
+		checkNotNull(value);
+		checkArgument(EMAIL_PATTERN.matcher(value).find(), "이메일 형식이 잘못 되었습니다.");
+
+		this.value = value;
+	}
+}
