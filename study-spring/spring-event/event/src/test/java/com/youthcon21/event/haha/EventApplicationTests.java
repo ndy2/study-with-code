dbe220d32751f3b46fd69d9fb6ff3d82ba493523
ndy2 <emrdbs12@gmail.com>
2022-09-19 16:40:49 +0900

    [spring-event] init: project setting

---
+++ study-spring/spring-event/event/src/test/java/com/youthcon21/event/haha/EventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youthcon21.event.haha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
