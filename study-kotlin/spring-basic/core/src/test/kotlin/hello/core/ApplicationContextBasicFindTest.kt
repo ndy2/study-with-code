652dce4709a2f204d43920653c3ee50b09c63838
ndy2 <emrdbs12@gmail.com>
2022-09-22 00:37:43 +0900

    [kotlin-springcorebasic] study: application context

---
+++ study-kotlin/spring-basic/core/src/test/kotlin/hello/core/ApplicationContextBasicFindTest.kt
@@ -0,0 +1,38 @@
+package hello.core
+
+import hello.core.member.service.MemberService
+import hello.core.member.service.MemberServiceImpl
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.NoSuchBeanDefinitionException
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+
+class ApplicationContextBasicFindTest {
+
+    private val ac = AnnotationConfigApplicationContext(AppConfig::class.java)
+
+    @Test
+    fun `빈 이름으로 조회`() {
+        val memberService = ac.getBean("memberService", MemberService::class.java)
+        assertThat(memberService).isInstanceOf(MemberServiceImpl::class.java)
+    }
+
+    @Test
+    fun `빈 이름으로 조회 실패`() {
+        assertThatExceptionOfType(NoSuchBeanDefinitionException::class.java)
+            .isThrownBy { ac.getBean("haha") }
+    }
+
+    @Test
+    fun `이름 없이 타입으로만 조회`() {
+        val memberService = ac.getBean(MemberService::class.java)
+        assertThat(memberService).isInstanceOf(MemberServiceImpl::class.java)
+    }
+
+    @Test
+    fun `구체 타임으로 조회`() {
+        val memberService = ac.getBean("memberService", MemberServiceImpl::class.java)
+        assertThat(memberService).isInstanceOf(MemberServiceImpl::class.java)
+    }
+}
---
+++ study-kotlin/spring-basic/core/src/test/kotlin/hello/core/ApplicationContextInfoTest.kt
@@ -0,0 +1,33 @@
+package hello.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.config.BeanDefinition
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+
+class ApplicationContextInfoTest {
+
+    private val ac = AnnotationConfigApplicationContext(AppConfig::class.java)
+
+    @Test
+    fun `모든 빈 출력하기`() {
+        val beanDefinitionNames = ac.beanDefinitionNames
+        for (beanDefinitionName in beanDefinitionNames) {
+            val bean = ac.getBean(beanDefinitionName)
+            println("beanDefinitionName = ${beanDefinitionName}, bean = ${bean}")
+        }
+    }
+
+    @Test
+    fun `애플리케이션 빈 출력하기`() {
+        val beanDefinitionNames = ac.beanDefinitionNames
+
+        for (beanDefinitionName in beanDefinitionNames) {
+            val beanDefinition = ac.getBeanDefinition(beanDefinitionName)
+            val bean = ac.getBean(beanDefinitionName)
+
+            if (beanDefinition.role == BeanDefinition.ROLE_APPLICATION) {
+                println("beanDefinitionName = ${beanDefinitionName}, bean = ${bean}")
+            }
+        }
+    }
+}
