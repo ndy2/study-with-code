e11865dbc26bb2cdf731144f1d51d8d311ee2c7f
ndy2 <emrdbs12@gmail.com>
2022-11-03 01:06:03 +0900

    [study-spring-swagger] 예외 처리 Schema 등록

---
+++ study-spring/study-swagger/src/main/java/com/example/studyswagger/controller/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.example.studyswagger.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.Data;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ApiResponse(
+			content = @Content(schema = @Schema(implementation = GlobalErrorResponse.class))
+	)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(RuntimeException.class)
+	public GlobalErrorResponse handleRuntimeException(Exception e) {
+		GlobalErrorResponse response = new GlobalErrorResponse();
+		response.errors.body = List.of(e.getMessage());
+
+		return response;
+	}
+
+	@Data
+	static class GlobalErrorResponse {
+
+		String hello;
+
+		@Schema(required = true)
+		Errors errors;
+
+		@Data
+		@Schema(hidden = true)
+		static class Errors {
+
+			@Schema(required = true)
+			List<String> body;
+		}
+	}
+}
